2433ceb5ee406f2c5d0c1e6d1c55eae79213b441
moh4b-z <mohammadsalim10000@gmail.com>
2024-03-12 23:38:09 -0300

    pronto menu

--- Visão Inicial/submenu.js
+++ Visão Inicial/submenu.js
@@ -1,37 +1,10 @@
-class MobileNavbar {
-    constructor(mobileMemu, menuL, navLinks) {
-        this.mobileMemu = document.querySelector(mobileMemu);
-        this.menuL = document.querySelector(menuL);
-        this.navLinks = document.querySelectorAll(navLinks);
-        this.aciveClass = "active";
+const btnMenu = document.getElementById('btn-menu')
 
-        this.handleClick = this.handleClick.bind();
-    }
-
-
-    handleClick() {
-        console.log(this);
-        this.menuL.classList.togglr(this.aciveClass);
-    }
-
-
-    addClickEvent() {
-        this.mobileMemu = addEventListener("click", this.handleClick);
-    }
-
-
-    init() {
-        if (this.mobileMemu) {
-            this.addClickEvent();
-        }
-        return this;
-    }
+function activeMenu() {
+    const nav = document.getElementById('nav')
+    nav.classList.toggle('active')
 
 }
 
-const mobileNavar = new MobileNavbar (
-    ".menu-re",
-    ".menu",
-    ".menu li",
-);
-mobileNavar.init();
+
+btnMenu.addEventListener('click', activeMenu)
